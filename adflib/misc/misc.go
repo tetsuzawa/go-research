cc29cdf9d90bab1665689903023c6de8d5288e9b
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-10-30 15:43:46 +0900

    Add Get Mean Error func

--- adflib/misc/misc.go
+++ adflib/misc/misc.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ElmAbs(fs []float64) []float64 {
-	for i, f := range fs{
+	for i, f := range fs {
 		fs[i] = math.Abs(f)
 	}
 	return fs
@@ -29,12 +29,25 @@ func RMSE(x1, x2 []float64) float64 {
 }
 
 func GetValidError(x1, x2 []float64) ([]float64, error) {
-	if len(x1) != len(x2){
-		err := errors.New("length of slices is different from n")
-		return nil,
+	if len(x1) != len(x2) {
+		err := errors.New("length of two slices is different")
+		return nil, err
 	}
+	floats.Sub(x1, x2)
+	e := x1
+	return e, nil
 }
 
-func GetMeanError(x1, x2 float64, fn string) {
-
+func GetMeanError(x1, x2 []float64, fn string) (float64, error) {
+	switch fn {
+	case "MAE":
+		return MAE(x1, x2), nil
+	case "MSE":
+		return MSE(x1, x2), nil
+	case "RMSE":
+		return RMSE(x1, x2), nil
+	default:
+		err := errors.New("")
+		return 0,
+	}
 }
