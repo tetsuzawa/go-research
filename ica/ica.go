c091894199a1831e14dbdea46ff7797523c7f3a7
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-02 19:15:05 +0900

    Add func whiten on ico

--- ica/ica.go
+++ ica/ica.go
@@ -1,12 +1,17 @@
 package ica
 
 import (
+	"errors"
 	"gonum.org/v1/gonum/floats"
 	"gonum.org/v1/gonum/mat"
+	"gonum.org/v1/gonum/stat"
+	"math"
 )
 
 type ICA struct {
-	xMat *mat.Dense
+	xMat    *mat.Dense
+	sampNum int
+	sigNum  int
 }
 
 func NewICA(x [][]float64) *ICA {
@@ -18,21 +23,55 @@ func NewICA(x [][]float64) *ICA {
 		}
 	}
 	ica.xMat = mat.NewDense(len(x), len(x[0]), x1d)
+	ica.sampNum, ica.sigNum = ica.xMat.Dims()
 	return ica
 }
 
-func (ica *ICA) CalcICA() [][]float64 {
+func (ica *ICA) CalcICA() ([][]float64, error) {
 	ica.fit()
-	z := ica.whiten()
+	z, err := ica.whiten()
+	if err != nil {
+		return nil, err
+	}
 	y := ica.analyze(z)
-	return y
+	return y, nil
 }
 
 func (ica *ICA) fit() {
-	r, c := ica.xMat.Dims()
-	sig := make([]float64, len(r))
-	for i := 0; i < c; i++ {
+	sig := make([]float64, ica.sampNum)
+	for i := 0; i < ica.sigNum; i++ {
 		mat.Col(sig, i, ica.xMat)
-		floats.Sub(sig, floats.)
+		sigMean := floats.Sum(sig) / float64(len(sig))
+		for j := 0; j < ica.sampNum; j++ {
+			sig[j] -= sigMean
+		}
+		ica.xMat.SetCol(i, sig)
 	}
+}
+
+func (ica *ICA) whiten() (*mat.Dense, error) {
+	sigma := mat.NewSymDense(ica.sigNum, nil)
+	stat.CorrelationMatrix(sigma, ica.xMat, nil)
+	var eigsym mat.EigenSym
+	ok := eigsym.Factorize(sigma, true)
+	if !ok {
+		return nil, errors.New("symmetric eigendecomposition failed")
+	}
+
+	// eigenvalues of sigma
+	D := eigsym.Values(nil)
+
+	// eigenvectors of sigma
+	var V *mat.Dense
+	eigsym.VectorsTo(V)
+
+	DhSl := make([]float64, ica.sigNum*ica.sigNum)
+	for i := 0; i < ica.sigNum; i++ {
+		DhSl[i*(ica.sigNum+1)] = math.Pow(D[i], -1/2)
+	}
+	Dh := mat.NewDense(ica.sigNum, ica.sigNum, DhSl)
+	retMat := produ
+
+
+
 }
