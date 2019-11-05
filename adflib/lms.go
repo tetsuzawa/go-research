43e9913cf94080ebc3e90fab7e1865f58b9490c7
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-05 15:41:30 +0900

    Fix lms to use mat.Dense

--- adflib/lms.go
+++ adflib/lms.go
@@ -28,10 +28,11 @@ func NewFiltLMS(n int, mu float64, w interface{}) (*FiltLMS, error) {
 }
 
 func (af *FiltLMS) Adapt(d float64, x []float64) {
-	y := floats.Dot(af.w, x)
+	y := floats.Dot(af.w.RawRowView(0), x)
 	e := d - y
+	w := af.w.RawRowView(0)
 	for i := 0; i < len(x); i++ {
-		af.w[i] += af.mu * e * x[i]
+		w[i] += af.mu * e * x[i]
 	}
 }
 
@@ -48,11 +49,12 @@ func (af *FiltLMS) Run(d []float64, x [][]float64) ([]float64, []float64, [][]fl
 	e := make([]float64, N)
 	//adaptation loop
 	for i := 0; i < N; i++ {
-		af.wHistory[i] = af.w
-		y[i] = floats.Dot(af.w, x[i])
+		w := af.w.RawRowView(0)
+		af.wHistory[i] = w
+		y[i] = floats.Dot(w, x[i])
 		e[i] = d[i] - y[i]
 		for j := 0; j < af.n; j++ {
-			af.w[j] = af.mu * e[i] * x[i][j]
+			w[j] = af.mu * e[i] * x[i][j]
 		}
 	}
 	return y, e, af.wHistory, nil
