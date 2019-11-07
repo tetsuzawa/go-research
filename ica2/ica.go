44d81db43bca5155467d8c6384ff3e0f9539d4f2
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-07 19:02:35 +0900

    Commit for tmp

--- ica2/ica.go
+++ ica2/ica.go
@@ -65,36 +65,65 @@ func Whiten(X *mat.Dense) (*mat.Dense, error) {
 }
 
 //CalcNewW calculate new w.
-//w is *mat.Dense [1, r]
+////w is *mat.Dense [1, r]
+////X is *mat.Dense [r, c]: [signul num, sample num]
+//w is *mat.Dense [c, 1]
 //X is *mat.Dense [r, c]: [signul num, sample num]
 func CalcNewW(w, X *mat.Dense) *mat.Dense {
 	r, c := X.Dims()
-	var auxSl1 = make([]float64, 1*c)
-	var auxSl2 = make([]float64, 1*c)
-	var auxSl3 = make([]float64, 1*c)
-	aux1 := mat.NewDense(1, c, nil)
-	aux1.Mul(w, X)
-	row1 := aux1.RawRowView(0)
+	//var auxSl1 = make([]float64, 1*c)
+	//var auxSl2 = make([]float64, 1*c)
+	//var auxSl3 = make([]float64, 1*c)
+	//aux1 := mat.NewDense(1, c, nil)
+	var auxSl1 = make([]float64, r)
+	var auxSl2 = make([]float64, r)
+	var auxSl3 = make([]float64, r)
+	//aux1 := mat.NewDense(r, 1, nil)
+	aux1 := mat.NewDense(1, r, nil)
+	////aux1.Mul(w, X)
+	//aux1.Mul(X, w)
+	aux1.Mul(w.T(), X.T())
+	//row1 := aux1.RawRowView(0)
+	//for i := 0; i < c; i++ {
+	//	auxSl1[i] = g(row1[i])
+	//	auxSl2[i] = gDer(row1[i])
+	//}
+	col1 := make([]float64, r)
+	mat.Col(col1, 0, aux1.T())
 	for i := 0; i < c; i++ {
-		auxSl1[i] = g(row1[i])
-		auxSl2[i] = gDer(row1[i])
+		auxSl1[i] = g(col1[i])
+		auxSl2[i] = gDer(col1[i])
 	}
 
 	aux3 := mat.NewDense(r, c, nil)
 	// diagonal matrix
-	for i := 0; i < r; i++ {
-		copy(auxSl3, X.RawRowView(i))
+	//for i := 0; i < r; i++ {
+	//	copy(auxSl3, X.RawRowView(i))
+	//	floats.Mul(auxSl3, auxSl1)
+	//	aux3.SetRow(i, auxSl3)
+	//}
+	for i := 0; i < c; i++ {
+		//copy(auxSl3, X.RawRowView(i))
+		mat.Col(auxSl3, i, X)
 		floats.Mul(auxSl3, auxSl1)
-		aux3.SetRow(i, auxSl3)
+		aux3.SetCol(i, auxSl3)
 	}
-	// [r, 1]
-	aux4 := ColMeanVector(aux3)
-	aux7 := mat.NewDense(1, r, nil)
+	//// [r, 1]
+	// [1, c]
+	//aux4 := ColMeanVector(aux3)
+	aux4 := RowMeanVector(aux3)
+	//aux7 := mat.NewDense(1, r, nil)
+	//aux7.Scale(SliceMean(auxSl2), w)
+	//wNew := mat.NewDense(1, r, nil)
+	//wNew.Sub(aux4.T(), aux7)
+	aux7 := mat.NewDense(c, 1, nil)
 	aux7.Scale(SliceMean(auxSl2), w)
-	wNew := mat.NewDense(1, r, nil)
+	wNew := mat.NewDense(c, 1, nil)
 	wNew.Sub(aux4.T(), aux7)
-	aux8 := make([]float64, 3)
-	copy(aux8, wNew.RawRowView(0))
+	aux8 := make([]float64, c)
+	//TODO
+	//copy(aux8, wNew.RawRowView(0))
+	mat.Col(aux8, 0, wNew)
 	for i, v := range aux8 {
 		aux8[i] = v * v
 	}
