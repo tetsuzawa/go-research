6349a00f04230970d1dd460e1256acc080acd7d5
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-23 13:50:11 +0900

    Add flag

--- ADF/raw_drone_convergence/explore_mu/main.go
+++ ADF/raw_drone_convergence/explore_mu/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
-	"os"
-	"path/filepath"
-	"strconv"
-
 	"github.com/tetsuzawa/go-adflib/adf"
 	research "github.com/tetsuzawa/go-research/ADF/raw_drone_convergence"
+	"os"
+	"path/filepath"
 )
 
 const (
@@ -18,17 +17,39 @@ const (
 
 func main() {
 
-	wavName := os.Args[1]
-	adfName := os.Args[2]
-	dataDir := os.Args[3]
+	//L, err := strconv.Atoi(os.Args[4])
+	//check(err)
+	var (
+		L       int
+		order   int
+		muStart float64
+		muEnd   float64
+		step    int
+	)
+	flag.IntVar(&L, "l", 64, "filter length")
+	flag.IntVar(&order, "order", 8, "order (AP)")
+	flag.Float64Var(&muStart, "start", 1e-5, "start value of mu")
+	flag.Float64Var(&muEnd, "end", 2, "end value of mu")
+	flag.IntVar(&step, "step", 100, "explore steps")
 
-	data := research.ReadDataFromWav(wavName)
-	L, err := strconv.Atoi(os.Args[3])
-	check(err)
+	flag.Parse()
+	fmt.Println("L:", L)
+	fmt.Println("order:", order)
+	fmt.Println("muStart:", muStart)
+	fmt.Println("muEnd:", muEnd)
+	fmt.Println("step:", step)
+
+	wavName := flag.Arg(0)
+	fmt.Println("wavName:", wavName)
+	adfName := flag.Arg(1)
+	fmt.Println("adfName:", adfName)
+	dataDir := flag.Arg(2)
+	fmt.Println("dataDir:", dataDir)
+
 	mu := 1.0
-	order, err := strconv.Atoi(os.Args[4])
-	check(err)
+	data := research.ReadDataFromWav(wavName)
 
+	var err error
 	var af adf.AdaptiveFilter
 	switch adfName {
 	case "LMS":
@@ -49,7 +70,7 @@ func main() {
 	d, x := research.MakeData(data, L)
 	fmt.Println("exploring mu ...")
 	//mu = ExploreLearning(d, x, af, testName, dataDir)
-	mu = research.ExploreLearning(d, x, af, 0.000001, 2, 100)
+	mu = research.ExploreLearning(d, x, af, muStart, muEnd, step)
 	af.SetStepSize(mu)
 
 	var testName string
