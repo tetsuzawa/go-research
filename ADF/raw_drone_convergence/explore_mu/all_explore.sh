a1121eb2b59aa57ac2e3053a0191c50ce1cbb6ec
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-25 11:37:58 +0900

    Fix import

--- ADF/raw_drone_convergence/explore_mu/all_explore.sh
+++ ADF/raw_drone_convergence/explore_mu/all_explore.sh
@@ -3,30 +3,30 @@
 #for algo in LMS NLMS AP RLS; do
 #  for len in 4 16 64 256 1024; do
 #    echo "${algo} start calculation with length ${len}"
-#    ./explore_mu_linux -l ${len} -start 0.000001 -end 2 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
+#    ./explore_mu -l ${len} -start 0.000001 -end 2 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
 #  done
 #done
 
 algo="LMS"
 for len in 4 16 64 256 1024; do
   echo "${algo} start calculation with length ${len}"
-  ./explore_mu_linux -l ${len} -start 0.000001 -end 2 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
+  ./build/explore_mu -l ${len} -start 0.000001 -end 2 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
 done
 
 algo="NLMS"
 for len in 4 16 64 256 1024; do
   echo "${algo} start calculation with length ${len}"
-  ./explore_mu_linux -l ${len} -start 0.000001 -end 2 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
+  ./build/explore_mu -l ${len} -start 0.000001 -end 2 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
 done
 
-#algo="AP"
-#for len in 4 16 64 256 1024; do
-#  echo "${algo} start calculation with length ${len}"
-#  ./explore_mu_linux -l ${len} -start 0.000001 -end 2 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
-#done
+algo="AP"
+for len in 4 16 64 256 1024; do
+  echo "${algo} start calculation with length ${len}"
+  ./build/explore_mu -l ${len} -start 0.000001 -end 30 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
+done
 
 algo="RLS"
 for len in 4 16 64 256 1024; do
   echo "${algo} start calculation with length ${len}"
-  ./explore_mu_linux -l ${len} -start 0.000001 -end 30 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
+  ./build/explore_mu -l ${len} -start 0.000001 -end 30 -step 100 ../wavfiles/dr_static_5.wav ${algo} ../jsonfiles
 done
--- ADF/raw_drone_convergence/run_adf/all_run_linux.sh
+++ ADF/raw_drone_convergence/run_adf/all_run_linux.sh
@@ -3,6 +3,6 @@
 for algo in LMS NLMS AP RLS; do
   for len in 4 16 64 256 1024; do
     echo "${algo} start calculation with length ${len}"
-    ./build/run_adf ../jsonfiles/${algo}_static_L-${len}.json ../csvfiles
+    ./build/run_linux ../jsonfiles/${algo}_static_L-${len}.json ../csvfiles
   done
 done
