b6b800e518965c11c50b51cb348330c5c59b3e80
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-11-27 10:50:56 +0900

    Add shellscript

--- ADF/raw_drone_convergence/csvfiles/all_plot.sh
+++ ADF/raw_drone_convergence/csvfiles/all_plot.sh
@@ -2,12 +2,21 @@
 
 /Users/tetsu/personal_files/Research/venv/bin/activate;
 
-for algo in LMS NLMS AP RLS; do
+for algo in LMS NLMS RLS; do
   for len in 4 16 64 256 1024; do
     echo "${algo} start to plot with length ${len}"
     python /Users/tetsu/personal_files/Research/research_tools/plot_from_csv.py ${algo}_static_L-${len}.csv -d ../imgfiles
     echo "\n\n\n"
   done
+done
+
+for algo in AP; do
+  for order in 8; do
+    for len in 4 16 64 256 1024; do
+      echo "${algo} start to plot with length ${len}"
+      python /Users/tetsu/personal_files/Research/research_tools/plot_from_csv.py ${algo}_static_L-${len}_order-${order}.csv -d ../imgfiles
+      echo "\n\n\n"
+    done
 done
 
 deactivate;
--- ADF/raw_drone_convergence/run_adf/all_run.sh
+++ ADF/raw_drone_convergence/run_adf/all_run.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env sh
 
-for algo in LMS NLMS AP RLS; do
+for algo in LMS NLMS RLS; do
   for len in 4 16 64 256 1024; do
     echo "${algo} start calculation with length ${len}"
     ./build/run_adf ../jsonfiles/${algo}_static_L-${len}.json ../csvfiles
+  done
+done
+
+for algo in AP; do
+  for order in 8; do
+    for len in 4 16 64 256 1024; do
+      echo "${algo} start calculation with length ${len}"
+      ./build/run_adf ../jsonfiles/${algo}_static_L-${len}_order-${order}.json ../csvfiles
+    done
   done
 done
