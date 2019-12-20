2f6dd32b743a797f5391d8ffdba79571b69fa9e2
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2019-12-20 14:58:26 +0900

    Add cmd

--- ADF/automatic_equalizer/run_adf_x_white_d_dr_voice_online/all_run.sh
+++ ADF/automatic_equalizer/run_adf_x_white_d_dr_voice_online/all_run.sh
@@ -8,7 +8,7 @@ DIR_NAME="auto_on"
 #for algo in LMS NLMS RLS; do
 for algo in NLMS RLS; do
   #for len in 4 16 64 256; do
-  for len in 4 256 1024 8096; do
+  for len in 4 16 64 128 256; do
     echo "${algo} start calculation with length ${len}"
     ./build/${BIN_NAME} ../jsonfiles/${DIR_NAME}/${algo}_static_L-${len}.json ../csvfiles/${DIR_NAME}
   done
@@ -17,7 +17,7 @@ done
 for algo in AP; do
   for order in 8; do
     #for len in 4 16 64 256; do
-    for len in 4 256 1024 8096; do
+    for len in 4 16 64 128 256; do
       echo "${algo} start calculation with length ${len}"
 	  ./build/${BIN_NAME} ../jsonfiles/${DIR_NAME}/${algo}_static_L-${len}_order-${order}.json ../csvfiles/${DIR_NAME}
     done
