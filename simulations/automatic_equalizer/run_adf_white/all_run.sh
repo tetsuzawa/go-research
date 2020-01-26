b23b13acbbbc68296cdcde7a525dfc63c9c8b1ba
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-01-26 09:27:52 +0900

    Fix script

--- simulations/automatic_equalizer/run_adf_white/all_run.sh
+++ simulations/automatic_equalizer/run_adf_white/all_run.sh
@@ -2,7 +2,8 @@
 
 #for algo in LMS NLMS RLS; do
 for algo in NLMS RLS; do
-  for len in 4 16 64 256; do
+  #for len in 4 16 64 256; do
+  for len in 4 ; do
     echo "${algo} start calculation with length ${len}"
     ./build/run_adf_white ../jsonfiles/white/${algo}_white_L-${len}.json ../csvfiles/white
   done
@@ -10,7 +11,8 @@ done
 
 for algo in AP; do
   for order in 8; do
-    for len in 4 16 64 256; do
+    #for len in 4 16 64 256; do
+    for len in 4 ; do
       echo "${algo} start calculation with length ${len}"
       ./build/run_adf_white ../jsonfiles/white/${algo}_white_L-${len}_order-${order}.json ../csvfiles/white
     done
